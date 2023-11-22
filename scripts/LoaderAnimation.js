98a5be36879d66e5f7ffd0e92e27de1a6f51b533
Anselme Massiot <mass.anselme@gmail.com>
2023-11-22 16:09:26 +0100

    feat : create the loader animation, modify style and the index

--- scripts/LoaderAnimation.js
+++ scripts/LoaderAnimation.js
@@ -1,3 +1,23 @@
 export class LoaderAnimation {
-     
+  constructor() {
+    this.loaderSection = document.getElementById("loaderSection");
+  }
+
+  showLoader() {
+    // set the first position of the loader container
+    this.loaderSection.classList.remove("hidden");
+  }
+
+  hideLoader() {
+    // add hiden class
+    this.loaderSection.classList.add("hidden");
+  }
+
+  initLoader() {
+    this.showLoader();
+
+    setTimeout(() => {
+      this.hideLoader();
+    }, 4000);
+  }
 }
