6fc949d2108c1e71c5f0452416403e79f1f852fb
Anselme Massiot <mass.anselme@gmail.com>
2023-11-23 14:08:00 +0100

    feat : create an class for the loader animation

--- scripts/LoaderAnimation.js
+++ scripts/LoaderAnimation.js
@@ -1,6 +1,20 @@
 export class LoaderAnimation {
   constructor() {
     this.loaderSection = document.getElementById("loaderSection");
+    this.mainSectionHeader = document.getElementById("mainSectionHeader");
+    this.home = document.getElementById("home");
+
+    // Add an event listener for the 'transitionend' event
+    this.loaderSection.addEventListener(
+      "transitionend",
+      this.onTransitionEnd.bind(this)
+    );
+  }
+
+  onTransitionEnd() {
+    // This function will be called when the transition is complete
+    this.mainSectionHeader.style.display = "flex";
+    this.home.style.display = "flex";
   }
 
   showLoader() {
@@ -9,11 +23,12 @@ export class LoaderAnimation {
   }
 
   hideLoader() {
-    // add hiden class
+    // add hidden class
     this.loaderSection.classList.add("hidden");
   }
 
   initLoader() {
+    // Add a class to trigger the transition
     this.showLoader();
 
     setTimeout(() => {
